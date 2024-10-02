ce466379b38b56b82095fb87e62948c27ec5cf8b
AMITABHA7430 <amitabhag17@gmail.com>
2024-10-03 00:13:08 +0530

    First_JS_commit

---
+++ jsprogs_git/day3.js
@@ -0,0 +1,26 @@
+let statusval = 3;
+
+switch(statusval)
+{
+    case 1:
+       {
+        console.log("super user");
+        break;
+    
+       }
+    case 2:
+    {
+        console.log("chutya");
+        break;
+    }
+    case 3:
+        {
+            console.log("chaman");
+            break;
+
+        }
+        case 4:
+            {
+                console.log("gandu");
+            }
+}
---
+++ jsprogs_git/script.js
@@ -0,0 +1,18 @@
+let num1 = 23;
+let num3 = 23;
+
+++num1;
+let connection = 4;
+if(connection===5)
+{
+console.log("i love ayaka");
+}
+else
+{console.log("i love raiden most")}
+
+console.log(num3);
+
+
+
+
+
